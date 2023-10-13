d1020927b0ad840e082a552aff905df06665f145
Julia <july.kucmierczyk@gmail.com>
2023-10-13 11:47:56 +0200

    Navbar buttons works now

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import Navbar from "./components/Navbar";
 import "../src/styles/App.css";
-import Cezar from "./components/Cezar";
-import Polibiusz from "./components/Polibiusz";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Cezar />
-      <Polibiusz />
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,48 @@
-const Navbar = () => {
-  // Create a navbar component that will be used to navigate between components
-  // If the use clicks on the button, the component should be changed
+import React from "react";
+import Cezar from "./Cezar";
+import Polibiusz from "./Polibiusz";
 
-  const showCezar = () => {
-    const cezar = document.querySelector(".cezar");
-    if (cezar) {
-      cezar.classList.remove("hidden");
-    }
+const Navbar = () => {
+  // if user clicks on a button, the state will change
+  const [state, setState] = React.useState("");
+
+  // create a function that will handle the click event
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const value = e.currentTarget.value;
+    setState(value);
   };
 
   return (
-    <nav className="navbar">
-      {/* Create three buttons that shows diffrent components */}
-      <button className="nav-btn tomato" onClick={showCezar}>
-        Szyfr Cezara
-      </button>
-      <button className="nav-btn blue">Szyfr Polibiusza</button>
-      <button className="nav-btn">Kolejny</button>
-    </nav>
+    <>
+      <nav className="navbar">
+        {/* Create three buttons that shows diffrent components */}
+        <button
+          className="nav-btn tomato"
+          onClick={(e: React.MouseEvent<HTMLButtonElement>) => handleClick(e)}
+          value={"Szyfr Cezara"}
+        >
+          Szyfr Cezara
+        </button>
+        <button
+          className="nav-btn blue"
+          onClick={(e: React.MouseEvent<HTMLButtonElement>) => handleClick(e)}
+          value={"Szyfr Polibiusza"}
+        >
+          Szyfr Polibiusza
+        </button>
+        <button
+          className="nav-btn"
+          onClick={(e: React.MouseEvent<HTMLButtonElement>) => handleClick(e)}
+          value={""}
+        >
+          Kolejny
+        </button>
+      </nav>
+      <section>
+        {state === "Szyfr Cezara" && <Cezar />}
+        {state === "Szyfr Polibiusza" && <Polibiusz />}
+      </section>
+    </>
   );
 };
 
